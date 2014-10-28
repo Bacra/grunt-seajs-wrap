3cb2778ca7408cda5ded6897c0eef573746322ea
Bacra Woo <bacra.woo@gmail.com>
2014-10-28 16:10:06 +0800

    Add: express js wrap engine

---
+++ express/seajs_wrap_engine.js
@@ -0,0 +1,25 @@
+// express js engin
+// example: app.engine('js', jsEngine);
+// note: add engine before set `public` path
+
+var fs = require('fs');
+
+module.exports = function jsEngine(file, options, fn)
+{
+	fs.readFile(file, function(err, content)
+	{
+		if (err)
+		{
+			fn(err);
+		}
+		else
+		{
+			fn(null,
+			[
+				'define(function(require, exports, module){',
+				content.toString(),
+				'});'
+			].join('\n'));
+		}
+	});
+};
